39618892f4d6d65e85b7032fceda95ffca695391
sohyeon <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>
2022-05-29 19:30:12 +0900

    행복주택 상세 정보 가져오기, gradle에 Geocoder 추가

--- hang-api/src/main/java/com/project/hang/mains/MainController.java
+++ hang-api/src/main/java/com/project/hang/mains/MainController.java
@@ -1,14 +1,19 @@
 package com.project.hang.mains;
 
+import com.google.code.geocoder.Geocoder;
+import com.google.code.geocoder.GeocoderRequestBuilder;
+import com.google.code.geocoder.model.GeocodeResponse;
+import com.google.code.geocoder.model.GeocoderRequest;
 import com.project.hang.api.LHApi;
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.io.IOException;
-
 
 @RestController
 @RequestMapping("/main")
@@ -20,18 +25,42 @@ public class MainController {
     }
 
     @GetMapping("/getAllList")
-    public ResponseEntity<String> getAllList() {
+    public ResponseEntity<String> getAllList() throws ParseException, IOException {
 
         // 행복주택인 항목만 리턴해주기
         // PAN_ID 구할 수 있음
         LHApi lhApi = new LHApi();
-        try {
-            String result = lhApi.getLhList();
-            JSONParser jsonParser = new JSONParser();
 
-        } catch (IOException e) {
-            e.printStackTrace();
+        String allResult = lhApi.getLhList();
+        JSONParser jsonParser = new JSONParser();
+        JSONArray jsonArray = (JSONArray)jsonParser.parse(allResult);
+        JSONObject jsonObject = (JSONObject) jsonArray.get(1);
+        JSONArray dsList = (JSONArray) jsonObject.get("dsList");
+
+        //AIS_TP_CD_NM이 행복주택인 항목만 꺼내기
+        JSONArray filterArray = new JSONArray();
+        for(int i=0;i<dsList.size();i++){
+            JSONObject obj = (JSONObject) dsList.get(i);
+            if(obj.get("AIS_TP_CD_NM").equals("행복주택")){
+                filterArray.add(dsList.get(i));
+            }
         }
+
+        //해당항목 상세 주소 구해주기
+        JSONArray addrArray = new JSONArray();
+        for(int i=0;i<filterArray.size();i++){
+            JSONObject info = (JSONObject) filterArray.get(i);
+            String addressInfo = lhApi.getLhHomeAdrees(info);
+            jsonArray = (JSONArray)jsonParser.parse(addressInfo);
+            JSONObject infoAddr = (JSONObject) jsonArray.get(1);
+            JSONArray list = (JSONArray) jsonObject.get("dsSch");
+            addrArray.add(list);//LGDN_ADR,LGDN_DTL_ADR
+        }
+
+        JSONArray returnArray = new JSONArray();
+        //Geocoder이용하여 위도, 경도 구해주기
+
+
 
         return ResponseEntity.ok("test");
     }
